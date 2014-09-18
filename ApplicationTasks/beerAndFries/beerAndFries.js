44f69a2567b65d56a7924f32bd65d8f14cffc5b8
planewalker <stefan.kovachev@gmail.com>
2014-09-18 12:18:49 +0300

    Solution to application task 1 added

---
+++ ApplicationTasks/beerAndFries/beerAndFries.js
@@ -0,0 +1,29 @@
+"use strict";
+
+var _ = require('../node_modules/lodash/lodash');
+
+var beerAndFries = function(testData)
+{
+    var beers = _.filter(testData, function(obj){
+            return obj.type === 'beer';
+        }),
+        fries = _.filter(testData, function(obj){
+            return obj.type === 'fries';
+        }),
+        beerScores = _.map(beers, function(beer){
+            return beer.score;
+        }),
+        friesScores = _.map(fries, function(fries){
+            return fries.score;
+        }),
+        sortedBeerScores = _.sortBy(beerScores),
+        sortedFriesScores = _.sortBy(friesScores);
+
+        // console.log(sortedBeerScores, sortedFriesScores);
+
+    return _.reduce(sortedBeerScores, function(result, num, key){
+        return result + (num * sortedFriesScores[key]);
+    }, 0);
+};
+
+exports.beerAndFries = beerAndFries;
