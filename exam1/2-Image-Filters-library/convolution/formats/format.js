a4a46a0083b7660856d8d54b9724b275aab4f820
planewalker <stefan.kovachev@gmail.com>
2014-11-12 17:12:39 +0200

    Updated convolution logic

--- exam1/2-Image-Filters-library/convolution/formats/format.js
+++ exam1/2-Image-Filters-library/convolution/formats/format.js
@@ -4,28 +4,55 @@ function Format() {
     
 }
 
-Format.prototype.applyKernel = function(imageData, kernel) {
-    var result = [],
-        deferred = Q.defer();
+function convulatePixel(position, imageData, kernel) {
+    // TODO do calcaulation -> http://www.songho.ca/dsp/convolution/convolution2d_example.html 
+    // TODO make sure it works for any kernel size / get its center
+    kernel.forEach(function(kernelRow, kernelRowIndex){
+        kernelRow.forEach(function(kernelRowElement, kernelRowElementIndex){
 
-    imageData.forEach(function(row, rowIndex){
-        row.forEach(function(pixelInRow, pixelInRowIndex){
-            // set accumulator to zero
-            kernel.forEach(function(kernelRow, kernelRowIndex){
-                kernelRow.forEach(function(kernelRowElement, kernelRowElementIndex){
-
-                    process.nextTick(function(){
-                        deferred.resolve(result);
-                        // calculate here
-                        // https://en.wikipedia.org/wiki/Kernel_%28image_processing%29
-                        // http://www.songho.ca/dsp/convolution/convolution2d_example.html example
-                    });
-
-                });
+            process.nextTick(function(){
+                deferred.resolve(result);
+                // calculate here
+                // https://en.wikipedia.org/wiki/Kernel_%28image_processing%29
+                // http://www.songho.ca/dsp/convolution/convolution2d_example.html example
             });
 
         });
     });
+}
+
+function convulateNextPixel(lastPixelPosition, imageData, kernel, result, deferred) {
+    var completed; // TODO: check of lastPixelPosition is out of bounds already -> resolve if yes
+    if (completed) {
+        deferred.resolve(result);
+    }
+    else {
+        process.nextTick(function(){
+            // TODO: calculate next position
+            var nextPosition;
+            // calculate
+            result[rowIndex][pixelInRowIndex] = convulatePixel(nextPosition, imageData, kernel);
+            // move to next
+            convulateNextPixel(nextPosition, imageData, kernel, result, deferred);
+        });
+    }
+}
+
+function createBlankResult(imageData) {
+    var result = [];
+    imageData.forEach(function(row, rowIndex){
+        row.forEach(function(pixelInRow, pixelInRowIndex){
+            result[rowIndex][pixelInRowIndex] = 0;
+        });
+    });
+    return result;
+}
+
+Format.prototype.applyKernel = function(imageData, kernel) {
+    var result = createBlankResult(imageData),
+        deferred = Q.defer();
+
+    convulateNextPixel([0, 0], imageData, kernel, result, deferred);
 
     return deferred.promise;
 };
